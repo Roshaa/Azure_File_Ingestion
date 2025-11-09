71568b8cb8e6f5cb62e7c09c51a8fd2b7129f0e5
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-11-09 00:29:01 +0000

    not anonymous function

--- fileingest-blob-upload/BlobUpload.cs
+++ fileingest-blob-upload/BlobUpload.cs
@@ -8,7 +8,7 @@ namespace fileingest_blob_upload;
 public class BlobUpload(ILogger<BlobUpload> _logger)
 {
     [Function("BlobUpload")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
     {
         var res = req.CreateResponse(HttpStatusCode.OK);
 
